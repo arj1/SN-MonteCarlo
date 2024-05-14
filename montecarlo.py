42fde195d1007315a096555131965111d07a0b5e
Anders Jerkstrand <anders.jerkstrand@astro.su.se>
2024-05-14 17:13:51 +0200

    Small fixes.

--- montecarlo.py
+++ montecarlo.py
@@ -3,50 +3,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.convolution import convolve, Gaussian1DKernel, Box1DKernel
 
-
-
 # Run parameters: ===================================
-N = 3E5 # Number of packets in simulation 1E5 default
+N = 3E5 # Number of packets in MC simulation
 type = 'linescattering' # 'electronscattering' or 'linescattering'
-Nlinesmax=2
-#rin = 0.5 # rout is at 1 : where is photosphere? (value 0 - 1)
 T = 5000 # in K
 v_out = 3000 # in km/s
-vlim = 1500 # For emission line: maximum emission velocity. For PCygni simulation: photospheric velocity.
-# ==Constants========================================
+vlim = 1500 # For emission line: maximum emission velocity. For P-Cygni simulation: photospheric velocity.
+lambda0 = 1000. 
+Nbin = 400 # Number of bins for creating emergent spectrum.
+# == Constants ========================================
 me = 9.11E-28
 mp = 1.67E-24
 k = 1.38E-16
 c = 3E5
-# ==Output format====================================
+# == Outputs ====================================
 save = 0 #save=0 --> plot on screen, 1 --> save to file.
 # ===================================================
 
 fig = plt.figure(figsize=(10,6))
 
 if (type == 'electronscattering'):
-    Nmodels = 2
-    tau0vec = [0.,1.0]  # For continuous simulations (elecronscattering)
-    Tvec = [0., 0.]
-    peakval = np.zeros(shape=(Nmodels,1)) # Initialization
-elif (type == 'linescattering'):
-    Nmodels = 2 # Number of runs:
-    tau0vec = np.zeros(shape=(Nmodels,Nlinesmax))
-    tau0vec[0,0:1] = [1E4]
-    tau0vec[1,0:1] = [1E4]
+    Nmodels = 2 # Number of models to compute
+    tau0vec = [0.,1.0]  # Radial optical depth to electron scattering.
+    Tvec = [0., 0.] # Temperatures
     #peakval = np.zeros(shape=(Nmodels,1)) # Initialization
+elif (type == 'linescattering'):
+    Nmodels = 2 # Number of models to compute
+    Nlinesmax=2 # Maximum number of lines in a model
+    tau0vec = np.zeros(shape=(Nmodels,Nlinesmax)) # Initialize
+    wllines = np.zeros(shape=(Nmodels,Nlinesmax)) # Initialize
+    destrprob = np.zeros(shape=(Nmodels,Nlinesmax)) # Initialize
+    tau0vec[0,0:1] = [1E4] # Line optical depths, model 1..
+    wllines[0,0:1] = [1000.] # Line wavelengths, model 1..
+    destrprob[0,0:1] = [0.] # Destruction probabilities model 1..
+    tau0vec[1,0:1] = [1E4] # Line optical depths, model 2..
+    wllines[1,0:1] = [1000.] # Line wavelengths, model 2..
+    destrprob[1,0:1] = [0.] # Destruction probabilities, model 2..
+    #peakval = np.zeros(sh ape=(Nmodels,1)) # Initialization
     #peakind = np.zeros(shape=(Nmodels,1)) # Initialization
     #xpeak = np.zeros(shape=(Nmodels,1)) # Initialization
-    wllines = np.zeros(shape=(Nmodels,Nlinesmax))
-    wllines[0,0:1] = [1000.]
-    wllines[1,0:1] = [1000.]
     #wllines[0,:] = np.arange(1003., 1050.,3.)
-    destrprob = np.zeros(shape=(Nmodels,Nlinesmax)) # epsilon
-    destrprob[0,0:1] = [0.] 
-    destrprob[1,0:1] = [0.] 
-
-lambda0 = 1000.
-Nbin = 400
 
 if (type == 'linescattering'):
 	wlmin = lambda0*(1-1.55*v_out/c)
@@ -81,56 +77,46 @@ for j in range(0,Nmodels):
 
         # Starting wavelength of packet:
         if (type == 'linescattering'):
-            wl = wlmin + (wlmax-wlmin)*random.random()
+            wl = wlmin + (wlmax-wlmin)*random.random() # A flat continuum
             mu = np.sqrt(random.random())
-            r = rin + 1E-5
+            r = rin + 1E-5 # All emission from just outside the photosphere
         elif (type == 'electronscattering'):
-            wl = lambda0
+            wl = lambda0 # A single line
             mu = 1. - 2*random.random()
-            r = rin*random.random()**(1./3.)
+            r = rin*random.random()**(1./3.) # Uniform sphere sampling
 
         sin_theta = np.sqrt(1. - mu**2)
 
         while (escaped == False and destroyed == False): # While packet is alive..
 
-            # Distance to outer shell edge:
+            # Compute distance to outer shell edge:
             distouter = -r*mu + np.sqrt((r*mu)**2 - (r**2 - 1**2))
 
-            # Distance to inner shell edge:
+            # Compute distance to inner shell edge:
             if ((type == 'linescattering') and (mu < 0 and r*sin_theta < rin)):
                 distinner = -r*mu - np.sqrt((r*mu)**2 - (r**2 - rin**2))
             else:
                 distinner = 2.
 
             # Draw random "lifetime" (=optical depth to absorption even) for this packet
-            z = random.random()
+            xi = random.random()
 
-            tau_thisphoton = -np.log(1-z) # TEMP righ log?
+            tau_thisphoton = -np.log(1-xi) # Draw lifetime for this packet
 
             distscatt = 2. # Initialize to a value > 1.
 
             if (type == 'electronscattering'):
 
-                #print "tau", tau_thisphoton
-
                 distscatt = tau_thisphoton/tau0
-
-                #print("distscatt",distscatt)
 
             elif (type == 'linescattering'):
 
-                # Find indeces of lines redward of current (co-moving) wavelength
+                # Find indeces of lines redward of current (co-moving) wavelength:
                 ind = (wllines[j,:] > wl)
-
-                #print("ind",ind, wllines[j,:])
-
            
-                if (any(ind)): # There is at least one more line remaining
-
-                    #print("apa", wllines[j,ind][0])
+                if (any(ind)): # There is at least one more line remaining..
 
                     distscatt = (wllines[j,ind][0]-wl)/wl*c/v_out # Distance to the closest line..
-                    #print("distscatt", distscatt)
 
                 else: # There is no redward line remaining
 
@@ -138,31 +124,25 @@ for j in range(0,Nmodels):
 
             sin_theta = np.sqrt(1.-mu**2)
 
-            #print "In", r, mu, distouter, wl
-
             if (distscatt < distouter and distscatt < distinner): # If distance to interaction is smaller than to both shell edges..
 
                 dr = distscatt
                 
                 r_nextpos = np.sqrt(r**2 + dr**2 - 2*r*dr*(-mu)) # Law of cosines for next radius..
 
-                #print("r", r, r_nextpos, mu)
-
                 gamma = 1./np.sqrt(1.-(dr*v_out/c)**2) # Gamma factor..
 
                 wl = wl*(1 + v_out/c*dr)*gamma    # New CMF wavelength (Rybicki Lightman eq 4.11 with cos_theta = -1 (homologous flow)).
-
-                #print("New CMF wl",wl)
                 
                 # Thermal reshuffling
-                z = random.random()
+                xi = random.random()
 
                 if (type == 'electronscattering'):
                     vthermal_use = np.sqrt(k*Tvec[j]/me)/1e5 # km/s
                 elif (type == 'linescattering'):
                     vthermal_use = np.sqrt(k*T/mp)/1e5 # km/s
 
-                dwl_thermal = wl*vthermal_use/c*(1.-2*z) # TEMP
+                dwl_thermal = wl*vthermal_use/c*(1.-2*xi) # TEMP
 
                 wl = wl + dwl_thermal
 
@@ -178,21 +158,19 @@ for j in range(0,Nmodels):
 
                 else: # TEMP rewrite this so that tau_accum >= tau_this has been checked already..
 
-                    x = random.random()
+                    xi = random.random()
 
-                    if (x < 1. - np.exp(-tau0vec[j,ind][0])): # Interaction happens..
+                    if (xi < 1. - np.exp(-tau0vec[j,ind][0])): # Interaction happens..
 
                         mu = 1. - 2*random.random()  # Isotropic scattering --> random new mu
 
                         sin_theta = np.sqrt(1.-mu**2) # Update sin(theta).. #TEMP needs to happen also for ES
 
-                        x = random.random()
+                        xi = random.random()
 
-                        if (x < destrprob[j,ind][0]): # Random draw if photon is thermalized..
+                        if (xi < destrprob[j,ind][0]): # Random draw if photon is thermalized..
 
-                            destroyed = True # TEMP
-                    
-                            #print "scattered", wl, r_nextpos
+                            destroyed = True # TEMP                    
 
                         else:
 
@@ -209,16 +187,12 @@ for j in range(0,Nmodels):
                         else:
                     
                             mu = np.sqrt(1.-sin_theta**2)
-
-                            # print "noscatter", wl, r_nextpos
                             
                 r = r_nextpos                    
 
             elif (distouter < distinner): # Next event is hitting outer shell edge..
 
                 dr = distouter
-
-                # print "distedge", distouter, distscatt, wl
 
                 # Comoving frame properties at edge
 
@@ -232,29 +206,19 @@ for j in range(0,Nmodels):
 
                 wl =  wl*(1 + v_out/c*dr)*gamma # Transformation to new CMF..
 
-                # In observer frame
+                # In observer frame:
 
                 gamma = 1./np.sqrt(1.-(v_out/c)**2)  
 
                 wl_escaped = wl*(1. - v_out/c*mu)*gamma  # Rybicki Lightman eq 4.11
 
-                #print "escaped", wl, mu, wl_escaped
-                
-                # Bin it!
+                # Bin it..
 
                 new = (binarr - wl_escaped)**2
-
-                #print new
         
                 ind = new.argmin()
 
-                #print ind
-
-                #print "Out", r_nextpos, mu, wl, wl_escaped, ind
-
-                #print ind
-
-                hist[ind] = hist[ind] + 1 # Add this packet to number of escaped in this (observer frame) wavelength bin..
+                hist[ind] = hist[ind] + 1 # Add this packet to number of escaped in this (observer frame) wavelength bin..TEMP energy of packet?
 
                 if (scattered):
 
@@ -270,87 +234,49 @@ for j in range(0,Nmodels):
 
     ynorm = 0.077
 
+    # Smooth MC noise:
     gauss_kernel = Gaussian1DKernel(2)
-    hist = convolve(hist, gauss_kernel) #
-    histscatt = convolve(histscatt, gauss_kernel) #
+    hist = convolve(hist, gauss_kernel) 
+    histscatt = convolve(histscatt, gauss_kernel) 
 
     if (j <= 2):
 
-        plt.plot((binarr-1000)/1000/(v_out/c),hist/N/ynorm*Nbin/50/0.26,color=colorvec[j],label=str(j),linewidth=2) # TEMP
+        plt.plot((binarr-lambda0)/lambda0/(v_out/c),hist/N/ynorm*Nbin/50/0.26,color=colorvec[j],label=str(j),linewidth=2) # TEMP
 
-        plt.plot((binarr-1000)/1000/(v_out/c),histscatt/N/ynorm*Nbin/50/0.26,'--',color=colorvec[j],label='scattered '+str(j),linewidth=1)
+        plt.plot((binarr-lambda0)/lambda0/(v_out/c),histscatt/N/ynorm*Nbin/50/0.26,'--',color=colorvec[j],label='scattered '+str(j),linewidth=1)
 
-        plt.plot((binarr-1000)/1000/(v_out/c),(hist-histscatt)/N/ynorm*Nbin/50/0.26,':',color=colorvec[j],label='unscattered '+str(j),linewidth=1)
-
-        #fid=open('banan'+str(j),'w')
-        #x=(binarr-1000)/1000/(v_out/c)
-        #y1=hist/N/ynorm*Nbin/50/0.26
-        #y2=histscatt/N/ynorm*Nbin/50/0.26
-        #np.savetxt(fid, np.c_[x,y1,y2])
-        #fid.close()
+        plt.plot((binarr-lambda0)/lambda0/(v_out/c),(hist-histscatt)/N/ynorm*Nbin/50/0.26,':',color=colorvec[j],label='unscattered '+str(j),linewidth=1)
 
     elif (j == 3):
 
-        plt.plot((binarr-1000)/1000/(v_out/c),hist/N/ynorm*Nbin/50/2,'--',color=colorvec[j-3])
+        plt.plot((binarr-lambda0)/lambda0/(v_out/c),hist/N/ynorm*Nbin/50/2,'--',color=colorvec[j-3])
 
     elif (j == 4):
 
-        plt.plot((binarr-1000)/1000/(v_out/c),hist/N/ynorm*Nbin/50/2,':',color=colorvec[j-3])
+        plt.plot((binarr-lambda0)/lambda0/(v_out/c),hist/N/ynorm*Nbin/50/2,':',color=colorvec[j-3])
 
     elif (j == 5):
 
-        plt.plot((binarr-1000)/1000/(v_out/c),hist/N/ynorm*Nbin/50/2,'-.',color=colorvec[j-3])
+        plt.plot((binarr-lambda0)/lambda0/(v_out/c),hist/N/ynorm*Nbin/50/2,'-.',color=colorvec[j-3])
         
 
     #peakval[j] = max(hist[0:int(Nbin/2)]/N/ynorm*Nbin/50)
     #peakind[j] = np.argmax(hist[0:int(Nbin/2)]/N/ynorm*Nbin/50)
     #xpeak[j] =  (binarr[peakind[j]]-1000.)/1000./(v_out/c)
     #print(j, peakval[j], peakind[j], (binarr[peakind[j]]-1000.)/1000./(v_out/c))
-
     #plt.plot((binarr-1000)/1000/(v_out/c), 0.06*(binarr[2]-binarr[1])/(40/50.)*(1-((binarr-1000)/10)**2))
 
 plt.ylim([0,2])
-plt.plot([-1.5,1.5],[1,1],'k--')
 plt.xlim([-1.5,1.5])
+
 plt.legend()
 
-
-#if (type == 'electronscattering'):
-#    plt.plot([0.48,0.55],[0.06/ynorm,0.065/ynorm],linewidth=5,'k')
-#    plt.text(0.56,0.066/ynorm,r'$\tau=0$')
-#    plt.plot([0.64,0.75],[0.035/ynorm,0.045/ynorm],'k')
-#    plt.text(0.8,0.046/ynorm,r'$\tau=1$')
-#    plt.plot([0.71,0.78],[0.028/ynorm,0.035/ynorm],'k')
-#    plt.text(0.82,0.036/ynorm,r'$\tau=2$')
-#    plt.plot([0.73,0.8],[0.024/ynorm,0.030/ynorm],'k')
-#    plt.text(0.8,0.031/ynorm,r'$\tau=3$')
-#else:
-    #plt.text(0.01,0.95,'Optically thin')
-    #plt.text(0.9,0.8,'Optically thick,')
-    #plt.text(0.9,0.75,'scatters')
-    #plt.text(0.01,0.6,'Optically thick,')
-    #plt.text(0.01,0.55,'destroyed')
-#plt.plot([xpeak[1],xpeak[1]],[peakval[1],peakval[1]+0.02],'k')
-#plt.plot([xpeak[2],xpeak[2]],[peakval[2],peakval[2]+0.02],'k')
-#plt.plot([xpeak[3],xpeak[3]],[peakval[3],peakval[3]+0.02],'k')
-#if (type == 'electronscattering'):
-#    plt.plot([-0.13,-0.13],[peakval[1],peakval[1]+0.02],'k')
-##    plt.plot([-0.20,-0.20],[peakval[2],peakval[2]+0.02],'k')
-#    plt.plot([-0.27,-0.27],[peakval[3],peakval[3]+0.02],'k')
-
-#    plt.text(xpeak[1]-0.1,peakval[1]+0.03,'-0.13')
-#    plt.text(xpeak[2]-0.1,peakval[2]+0.03,'-0.20')
-#    plt.text(xpeak[3]-0.1,peakval[3]+0.03,'-0.27')
-
-
-
+plt.plot([-1.5,1.5],[1,1],'k--')
 plt.plot([0,0],[0,1],'k--')
 plt.plot([-1,-1],[0,1],'k--')
 
 plt.xlabel('Wavelength ($\Delta \lambda/\lambda_0/(V_{max}/c)$)')
 plt.ylabel('Flux')
-
-#plt.yscale('log')
 
 if (save == 0):
     plt.show()
@@ -358,7 +284,6 @@ else:
     if (type == 'electronscattering'):
         plt.savefig("electronscattering.eps",bbox_inches='tight')
     else:
-        plt.xlim([-1,1.5])
         plt.savefig("linescattering.eps",bbox_inches='tight')
         
  
